9b249f480358067398a74ce3f8bb67b0b7350bc6
matteomiceli <omatteomiceli@gmail.com>
2022-11-14 17:11:57 -0800

    weapon fire_rate

--- game/src/Characters/Player/BrideController.gd
+++ game/src/Characters/Player/BrideController.gd
@@ -3,11 +3,14 @@ extends KinematicBody2D
 export var move_speed: int = 100
 var velocity: Vector2
 
-# bullet
+# weapon
 var bullet = preload('res://src/Objects/Projectiles/Bullet.tscn')
 var bullet_speed = 1500
+var time_since_last_shot = 1 # should start incrementing at fire_rate
+var fire_rate = 1 # bullets per second
 
-func _process(_delta):
+func _process(delta):
+	time_since_last_shot += delta
 	directionalMovement()
 	fireWeapon()
 	
@@ -25,7 +28,10 @@ func directionalMovement ():
 
 func fireWeapon():
 	if Input.get_action_strength("weapon_fire"):
-		var bullet_instance = bullet.instance()
-		bullet_instance.position = global_position
-	
-		get_parent().add_child(bullet_instance)
+		if time_since_last_shot > fire_rate:
+			time_since_last_shot = 0
+			var bullet_instance = bullet.instance()
+			bullet_instance.position = global_position
+			
+			get_parent().add_child(bullet_instance)
+			
