29ac7f1b5d4de5eda7205d18ff0fb3d03196d4bc
Jamie Samuel <jsam07@hotmail.com>
2022-11-17 18:25:43 -0800

    Add sprite movement w/ weapon

---
+++ game/src/Characters/Player/Bride/ScreenShaker.gd
@@ -0,0 +1,26 @@
+extends Node
+
+export (int) var time = 0
+onready var camera = get_parent()
+
+func _ready():
+	pass 
+	
+	
+func _shake(duration := 0.8, magnitude := 8):
+	
+	while time < duration:
+		time += get_process_delta_time()
+		time = min(time, duration)
+		
+		var offset = Vector2()
+		
+		offset.x = rand_range(-magnitude, magnitude)
+		offset.y = rand_range(-magnitude, magnitude)
+		
+		camera.set_offset(offset)
+		
+		yield(get_tree(), "idle_frame")
+		
+	time = 0
+	camera.set_offset(Vector2.ZERO)
